b933b3e8ae01451e1cd3bdea74755a7572ca1467
KrausRaus <33986551+KrausRaus@users.noreply.github.com>
2026-02-11 07:14:11 -0500

    Update isqrt.php

--- isqrt.php
+++ isqrt.php
@@ -1,1 +1,34 @@
+<!DOCTYPE html>
+<html>
 
+ <script>
+
+  function isqrt(){
+
+  // document.getElementById("answerBox").innerHTML = "";
+
+  // let numero = document.getElementById("numberBox").innerHTML;
+
+   //if(!(Number.isNan(1/numero))  ){  document.getElementById("answerBox").innerHTML = Math.floor(Math.sqrt(numero));    }else{ document.getElementById("numberBox").innerHTML = "";     alert("THAT IS NOT A NUMBER, IDIOT");}
+
+   //document.getElementById("answerBox").innerHTML = numero;
+
+   document.getElementById("answerBox").innerHTML = document.getElementById("numberBox").innerHTML;
+   
+  }
+  
+ </script>
+
+ <body>
+
+PUT A NUMBER IN THE BOX<br>
+PUSH THE GO BUTTON<br>
+YOU WILL SEE THE INTEGER SQAURE ROOT OF THE NUMBER<br>
+
+ <input id="numberBox"></input> <button onclick="isqrt()">GO</button> <br><br>
+
+ <div id="answerBox"></div>
+
+</body>
+  
+</html>
